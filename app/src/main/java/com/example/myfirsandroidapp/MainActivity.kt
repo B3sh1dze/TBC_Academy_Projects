ab26fa4adb07a57500cf8e2aabc70c52973f322e
B3sh1dze <120786832+B3sh1dze@users.noreply.github.com>
2024-12-09 10:30:13 +0400

    Update

--- app/src/main/java/com/example/myfirsandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirsandroidapp/MainActivity.kt
@@ -9,16 +9,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myappforworking.databinding.ActivityMainBinding
 
-/*import com.example.myappforworking.databinding.ActivityMainBinding
-import com.example.myappforworking.databinding.ActivityMainBinding
-import com.example.myfirsandroidapp.databinding.ActivityMainBinding*/
-
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
-    private val userMap = mutableMapOf<String, String>()
+    private val anagrams = mutableListOf<String>()
+    private val lengthToStringsMap = mutableMapOf<Int, MutableList<String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,58 +21,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val addUserBtn = binding.addUserBtn
-        val searchEmailBtn = binding.searchEmailBtn
+        val saveBtn = binding.anagramSaveBtn
+        val outputBtn = binding.anagramOutputBtn
+        val clearBtn = binding.clearBtn
 
-        addUserBtn.setOnClickListener {
-            val fullName = binding.etFullNameInput.text.toString()
-            val email = binding.etEmailInput.text.toString()
-            if (isFullNameValid(fullName) && isEmailValid(this, email)) {
-                userMap[email] = fullName
-                binding.tvUsersCount.text = "Users -> " + userMap.size.toString()
-                binding.etFullNameInput.text?.clear()
-                binding.etEmailInput.text?.clear()
+        saveBtn.setOnClickListener {
+            val anagram = binding.anagramEnterBtn.text.toString()
+            if (anagram.isEmpty()) {
+                Toast.makeText(this, "Anagram field is empty", Toast.LENGTH_SHORT).show()
             }
-        }
-        searchEmailBtn.setOnClickListener {
-            val searchEmail = binding.etSearchEmail.text.toString()
-            val showUserInfo = binding.showUserInfo
-            if (searchEmail.isEmpty()) {
-                Toast.makeText(this, "Email field is empty", Toast.LENGTH_SHORT).show()
-            } else if (userMap.containsKey(searchEmail)) {
-                val fullName = userMap[searchEmail]
-                showUserInfo.text = "User -> $fullName \n Email -> $searchEmail"
-            } else {
-                showUserInfo.text = "User not found"
+            anagrams.add(anagram)
+            for (anagram in anagrams) {
+                val anagramLength = anagram.length
+                if(lengthToStringsMap.containsKey(anagramLength)) {
+                    lengthToStringsMap[anagramLength]?.add(anagram)
+                } else {
+                    lengthToStringsMap[anagramLength] = mutableListOf(anagram)
+                }
             }
-            binding.etSearchEmail.text?.clear()
+            binding.anagramEnterBtn.text?.clear()
         }
-    }
-    private fun isEmailValid(context: Context, email: String): Boolean {
-        val emailPattern = "[a-z0-9._%+-]+@[a-z0-9.]+.[a-z]"
-        val emailRegex = Regex(emailPattern)
-        if (email.isEmpty()) {
-            Toast.makeText(this, "Email field is empty", Toast.LENGTH_SHORT).show()
-            return false
-        } else if (emailRegex.matches(email)) {
-            return true
-        } else {
-            Toast.makeText(this, "Email format is invalid.", Toast.LENGTH_SHORT).show()
-            return false
+        outputBtn.setOnClickListener {
+            binding.tvShowAnagrams.text = lengthToStringsMap.toString()
         }
-    }
 
-    private fun isFullNameValid(fullName: String): Boolean {
-        val fullNamePattern = "^[A-Z][a-z]+( [A-Z][a-z]+)+$"
-        val fullNameRegex = Regex(fullNamePattern)
-        if (fullName.isEmpty()) {
-            Toast.makeText(this, "Name field is empty", Toast.LENGTH_SHORT).show()
-            return false
-        } else if (fullNameRegex.matches(fullName)) {
-            return true
-        } else {
-            Toast.makeText(this, "Name format is invalid.", Toast.LENGTH_SHORT).show()
-            return false
+        clearBtn.setOnClickListener {
+            anagrams.clear()
+            lengthToStringsMap.clear()
+            binding.tvShowAnagrams.text = ""
         }
     }
 }
